7ef543fa946e7db4363715f19e45fd1b0f4280b0
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-07-30 17:46:18 +0600

    using jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000;
 //middle ware
 app.use(cors());
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
@@ -31,6 +49,15 @@ async function run() {
     const userCollection = client.db("RentopiaDB").collection("users");
     const propertyCollection = client.db("RentopiaDB").collection("properties");
     const wishListCollection = client.db("RentopiaDB").collection("wishLists");
+
+    // Jwt related api
+
+    app.post('/jwt',(req,res)=>{
+      const user=req.body;
+      const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' });
+      res.send({token})
+    })
+
 
     // user related api
 
@@ -106,10 +133,14 @@ async function run() {
 
     // wishlist collection
 
-    app.get('/wishLists', async (req, res) => {
+    app.get('/wishLists',verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
+      }
+      const decodedEmail=req.decoded.email;
+      if(email !==decodedEmail){
+        return res.status(403).send({error:true, message:'forbidden access'})
       }
       const query = { email: email };
       const result = await wishListCollection.find(query).toArray();
