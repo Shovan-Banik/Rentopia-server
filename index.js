b47c15502cc904a14416c44a6d1ea0587022e44c
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-08-01 23:55:24 +0600

    ssl payment gateway implement

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const SSLCommerzPayment = require('sslcommerz-lts')
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -41,6 +42,11 @@ const client = new MongoClient(uri, {
 });
 
 
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false //true for live, false for sandbox
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -49,6 +55,8 @@ async function run() {
     const userCollection = client.db("RentopiaDB").collection("users");
     const propertyCollection = client.db("RentopiaDB").collection("properties");
     const wishListCollection = client.db("RentopiaDB").collection("wishLists");
+    const bookingCollection = client.db("RentopiaDB").collection("bookings");
+    
 
     // Jwt related api
 
@@ -61,7 +69,7 @@ async function run() {
 
     // user related api
 
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyJWT,async(req,res)=>{
       const result=await userCollection.find().toArray();
       res.send(result);
     })
@@ -160,6 +168,87 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await wishListCollection.deleteOne(query);
       res.send(result);
+    })
+
+    // booking related api
+    app.post('/bookings', async(req,res)=>{
+      console.log(req.body);
+      const booking=req.body;
+
+      const selectedProperty=await wishListCollection.findOne({_id : new ObjectId(req.body.productId)});
+      console.log(selectedProperty);
+
+      const tran_id=new ObjectId().toString();
+
+      const data = {
+        total_amount: selectedProperty?.price,
+        currency: booking.currency,
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: selectedProperty.category,
+        product_profile: 'general',
+        cus_name: booking.name,
+        cus_email: booking.email,
+        cus_add1: booking.location,
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: booking.phone,
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+
+    console.log(data);
+
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({url:GatewayPageURL});
+
+        const finalOrder={
+          selectedProperty,
+          paidStatus:false,
+          transactionId:tran_id
+        }
+        const result=bookingCollection.insertOne(finalOrder);
+
+
+        console.log('Redirecting to: ', GatewayPageURL)
+    });
+    app.post('/payment/success/:tranId',async(req,res)=>{
+      console.log(req.params.tranId);
+      const result=await bookingCollection.updateOne({transactionId:req.params.tranId},{
+        $set:{
+          paidStatus:true
+        },
+      })
+      if(result.modifiedCount>0){
+        res.redirect(`http://localhost:5173/dashboard/payment/success/${req.params.tranId}`)
+      }
+
+    })
+
+    app.post('/payment/fail/:tranId',async(req,res)=>{
+      const result=await bookingCollection.deleteOne({transactionId:req.params.tranId});
+      if(result.deletedCount){
+        res.redirect(`http://localhost:5173/dashboard/payment/fail/${req.params.tranId}`)
+      }
+    })
+
     })
 
 
